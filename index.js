9a7f9d7ed43790f5a101faaa7e4bff50af1a493c
unknown <angeloev@gmail.com>
2025-07-07 12:01:59 +0300

    index.html

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.sayTitle = function() {
+        console.log(this.title,'by', this.author, this.pages, 'pages',this.read)
+    }
+}
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295','not read yet');
+
+book1.sayTitle()
+*/
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
