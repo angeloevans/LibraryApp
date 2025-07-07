53edd1dfe86504e2505043f2ad7cbf2115c4ae1e
unknown <angeloev@gmail.com>
2025-07-07 14:45:22 +0300

    Update the Js fucntion, still delte buttons must function more  than once!

--- index.js
+++ index.js
@@ -31,44 +31,61 @@ const myLibrary = [
   }
 ];
 
-// the constructor...
 function Book(title, author, pages, read) 
 {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
-
 function displayBooks() {
-  const container = document.getElementById('details');
-  container.innerHTML = '';
-
-  myLibrary.forEach(book => {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-
+  const container = document.getElementById('details')
+  container.innerHTML = ''
+  myLibrary.forEach((book, index) => {
+    const bookCard = document.createElement('div')
+    bookCard.classList.add('book-card')
     bookCard.innerHTML = `
-      <h3>${book.title}</h3>
+      <h3 class="book-title"> ${book.title}</h3>
       <p><strong>Author:</strong> ${book.author}</p>
       <p><strong>Pages:</strong> ${book.pages}</p>
       <p><strong>Status:</strong> ${book.read ? "Read ✅" : "Not Read ❌"}</p>
+      <button class="delete-button" data-index="${index}">Delete from Library</button>
     `;
 
     container.appendChild(bookCard);
   });
 }
 
+function deleteButtons() {
+  const deleteButtons = document.querySelectorAll('.delete-button');
+    deleteButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const bookIndex = parseInt(button.getAttribute('data-index'), 10);
+        myLibrary.splice(bookIndex, 1);
+        
+        console.log("Book index is deleted!",bookIndex)
+        displayBooks();
+      });
+    });
+  }
 
-window.addEventListener('DOMContentLoaded', () => {
-  displayBooks();
-});
 
-
-/*
 function addBookToLibrary() {
-  // take params, create a book then store it in the array
+  const author = document.getElementById('author').value
+  const title = document.getElementById('title').value
+  const pages = parseInt(document.getElementById('pages').value, 10)
+  const read = document.getElementById('read').checked
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
+  console.log(myLibrary, "Library:")
+  console.log("I entered New Values:", author, title, pages, read)
+  displayBooks()
+  document.querySelector('.addBook').reset()
+  return false
 }
 
-*/
+document.getElementById('addBtn').addEventListener('click', addBookToLibrary)
+
+displayBooks()
+deleteButtons()
